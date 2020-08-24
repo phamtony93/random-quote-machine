c91cb44f781cfbb5bb383b9fddf7e94cadba2cef
Tony Pham <topham@adobe.com>
2020-08-24 14:50:38 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+// import Button from 'react-bulma-components/lib/components/button';
+// import Columns from 'react-bulma-components/lib/components/columns';
+// // import Column from 'react-bulma-components/lib/components/';
+// import Container from 'react-bulma-components/lib/components/container'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function ExternalButton() {
+  return (
+    <Button className="is-primary">Go</Button>
+  )
+}
 
 function App() {
+  let [hasQuote, setHasQuote] = useState(false)
+  let [quote, setQuote] = useState({
+    contents: 'test',
+    title: 'test',
+    link: ''
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="app">
+      <Row>
+        quote
+        {quote.contents}
+      </Row>
+      <Row>
+        <Button>Tweet</Button>
+        <Button>Tumblr</Button>
+        <Button onClick={changeQuote} >New Quote</Button>
+      </Row>
+    </Container>
   );
 }
+
+let changeQuote = () => {
+  const END_POINT = 'https://quotesondesign.com/wp-json/wp/v2/posts/'
+  const request = new Request(END_POINT)
+  fetch(request)
+    .then(response => response.json())
+    .then(data =>  {
+      console.log(data)
+    })
+}
+// displayContents = () => {
+//   if (hasQuote) {
+//     return (
+//       <div>
+
+//       </div>
+//     )
+//   } else {
+//     return (
+      
+//     )
+//   }
+// }
 
 export default App;
