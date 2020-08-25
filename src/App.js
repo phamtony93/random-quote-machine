43d10f5eefa1c1d62af6a36724e683a6e1f3ca4d
Tony Pham <topham@adobe.com>
2020-08-24 22:34:21 -0700

    added capability to share to twitter and tumblr

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function ExternalButton() {
-  return (
-    <Button className="is-primary">Go</Button>
-  )
-}
+// function ExternalButton() {
+//   return (
+//     <Button className="is-primary">Go</Button>
+//   )
+// }
 
 function App() {
   let [hasQuote, setHasQuote] = useState(false)
@@ -50,18 +50,43 @@ function App() {
       })    
   }
 
+  // return (
+  //   <Container id="app">
+  //     <Row>
+  //       {displayContents(hasQuote, quote)}
+  //     </Row>
+  //     <Row>
+  //       <Button>Tweet</Button>
+  //       <Button>Tumblr</Button>
+  //       <Button onClick={getQuote} >New Quote</Button>
+  //     </Row>
+  //   </Container>
+  // );
   return (
     <Container id="app">
-      <Row>
-        {displayContents(hasQuote, quote)}
-      </Row>
-      <Row>
-        <Button>Tweet</Button>
-        <Button>Tumblr</Button>
-        <Button onClick={getQuote} >New Quote</Button>
-      </Row>
+      <div id="quote-machine">
+        <Col className="align-self-center">
+          <Row className="justify-content-center">
+              {displayContents(hasQuote, quote)}   
+          </Row>
+          <Row className="justify-content-center">
+            <Button className="btn-secondary" onClick={() => postTweet(quote.contents, quote.title)}>Tweet</Button>
+            <Button className="btn-secondary" onClick={() => postTumblr(quote.contents, quote.title)}>Tumblr</Button>
+            <Button onClick={getQuote} className="btn-secondary">New Quote</Button>
+          </Row>
+        </Col>
+      </div>
+
     </Container>
-  );
+  );  
+}
+const postTweet = (quote, author) => {
+  window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quote + '" ' + author));
+}
+
+const postTumblr = (quote, author) => {
+  window.open('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(author)+'&content=' 
+    + encodeURIComponent(quote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
 }
 
 const removeHtmlFromString = (string) => {
@@ -71,14 +96,17 @@ const removeHtmlFromString = (string) => {
 const displayContents = (hasQuote, quote) => {
   if (hasQuote) {
     return (
-      <div>
-        {quote.contents}
-        {quote.title}
+      <div className="contents">
+        "{quote.contents}
+        <div id="author">
+          {quote.title}
+        </div>
       </div>
+
     )
   } else {
     return (
-      <div>
+      <div className="contents">
         Please generate a new quote
       </div>
     )
